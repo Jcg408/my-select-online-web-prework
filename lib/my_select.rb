f8e462bc7ba2042713f6b3dcfa819b270153dc30
Jessica Gladstone <jcg321@comcast.net>
2018-10-02 17:14:25 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i =0 
   items = []
   while i < collection.length
-    yield(collection[i])
+   x = yield(collection[i])
     i+=1 
     items<< collection[i] == true
   end
