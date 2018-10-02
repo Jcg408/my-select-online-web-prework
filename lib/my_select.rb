dce755b04bbd530dc8aaacda56c7eb477e5b395a
Jessica Gladstone <jcg321@comcast.net>
2018-10-02 17:14:59 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
   while i < collection.length
    x = yield(collection[i])
     i+=1 
-    items<< collection[i] == true
+    items<< collection[i] if x == true
   end
   
  end
